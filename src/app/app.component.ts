ea289914563b8d3fc98ff34220c039dd298a0a15
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2024-10-28 03:42:38 -1200

    Design register form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , MatAutocompleteModule ,MatButtonModule,MatFormFieldModule,MatInputModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        component:SignupComponent,
+        pathMatch:'full',
+    },
+];
