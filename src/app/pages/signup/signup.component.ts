04d4f373e7c0a71bfb147f57e7a0d51d0a6c46e6
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2024-11-05 01:25:08 -1200

    Form design complete

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import {  MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,MatLabel,MatInputModule,MatButtonModule ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
