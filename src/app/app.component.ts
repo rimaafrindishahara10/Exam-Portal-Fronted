a5edcace37cc81197b8bc73e8bb7aa98d262b8b1
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2024-11-14 01:37:49 -1200

    Navbar work done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { HttpClientModule } from '@angular/common/http';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , MatAutocompleteModule ,MatButtonModule,MatFormFieldModule,MatInputModule , HttpClientModule,MatSnackBarModule,MatCardModule],
+  imports: [RouterOutlet, MatIconModule, MatAutocompleteModule, MatButtonModule, MatToolbarModule, MatFormFieldModule, MatInputModule, HttpClientModule, MatSnackBarModule, MatCardModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatIcon,MatToolbar, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
