50e1c07428514cf6327aef24be5a492c57eb292c
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2024-11-14 02:16:06 -1200

    Styling done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButton],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
